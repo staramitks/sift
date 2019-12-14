aa3e2e7be9f88536e511a899e129d95e8d3d38f8
Amit Singh <star.amitks@gmail.com>
2019-12-14 21:50:21 +0530

    Initial commit

---
+++ src/main/java/com/sift/app/SiftApp.java
@@ -0,0 +1,14 @@
+package com.sift.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiftApp {
+	public static void main(String[] args) {
+		SpringApplication.run(SiftApp.class, args);
+	}
+}
+
+
+
